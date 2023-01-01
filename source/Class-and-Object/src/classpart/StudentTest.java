451b52831362a8bc6e80cbc072de719ca35f9f05
shinwoos <kmnj2100@gmail.com>
2023-01-01 22:42:19 +0900

    text update

---
+++ source/Class-and-Object/src/classpart/StudentTest.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class StudentTest {
+	
+	public static void main(String[] args) {
+		Student studentLee = new Student();
+		studentLee.studentName = "김김김";
+		studentLee.address = "어쩌시 어쩌구 어쩌동";
+		
+		studentLee.showStudentInfor();
+	}
+
+}
